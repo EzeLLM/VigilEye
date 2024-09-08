24534dc71ed3183d8f772259bef939ce4d650650
Mert ERGÜN <mertergun17@hotmail.com>
2024-09-08 21:48:30 +0300

    Add gemini support

--- dev/VigilantClassifier.py
+++ dev/VigilantClassifier.py
@@ -1,46 +1,36 @@
-import argparse
-from jinja2 import Environment, FileSystemLoader
+import os
+from dotenv import load_dotenv
+from PromptGen import PromptGen
+from LLMModels import get_llm_model
 import Common as common
 from SharedLogger import CustomLogger
-from typing import final
-from openai import OpenAI
-from PromptGen import PromptGen
-import os
-from typing import Optional
-from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 logger = CustomLogger().get_logger()
 
-
-class VigilantClassifier():
-    def __init__(self,config_dir, template, main_field, other_fields,handle_pics=False, input = False):
+class VigilantClassifier:
+    def __init__(self, config_dir, template, main_field, other_fields, handle_pics=False, input=False):
         self.template = template
-        self.config = common.OpenYaml(config_dir,'classifier')
-        self.api_key = os.getenv('OPENAIAPIKEY')
-        self.model = self.config['model']
-        self.client = OpenAI(api_key=self.api_key)
+        self.config = common.OpenYaml(config_dir, 'classifier')
+        self.api_key = os.getenv('LLM_API_KEY')
+        self.model_type = self.config['model_type']
+        self.model_name = self.config['model_name']
+        self.llm_model = get_llm_model(self.model_type, self.api_key, self.model_name)
         self.input = input
-        self.PromptGet = PromptGen(config_path=config_dir,input=self.input,main_field=main_field,other_fields=other_fields,handle_pics=handle_pics,template=template)
+        self.PromptGet = PromptGen(config_path=config_dir, input=self.input, main_field=main_field, other_fields=other_fields, handle_pics=handle_pics, template=template)
 
-
-    def ModelResponse(self,history,prompt=''):
+    def ModelResponse(self, history, prompt=''):
         if prompt:
             history.append({'role': 'user', 'content': prompt})
-        response =  self.client.chat.completions.create(
-            model = self.model,
-            messages = history,
-            temperature = 0.3
-        )
+        response = self.llm_model.generate_response(history)
+        history.append({'role': 'assistant', 'content': response})
+        return response, history
 
-        return response.choices[0].message.content, history.append({'role':'assistant','content':response.choices[0].message.content})
-    
-    def GetReport(self,):
+    def GetReport(self):
         prompt = self.PromptGet.ConstructPrompt()
         history = common.OpenJson(self.config['history_path'])
-        response ,_ = self.ModelResponse(history=history,prompt=prompt)
+        response, _ = self.ModelResponse(history=history, prompt=prompt)
         return response
-    
 
 if __name__ == "__main__":
     vc = VigilantClassifier()
