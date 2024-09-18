3e197dde56b54e7e0db2f326af6f8d5ce6e55530
Ezel Bayraktar <BNUBNU20@OUTLOOK.COM>
2024-09-19 00:11:41 +0300

    removed need for pytorch when not using local image interp

--- dev/ImageInterpreter.py
+++ dev/ImageInterpreter.py
@@ -1,32 +1,31 @@
-import torch
 import requests
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
 import Common as common
-from openai import OpenAI
-import matplotlib.image as mpimg
-import dotenv
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 import warnings
 warnings.filterwarnings("ignore", message="huggingface/tokenizers: The current process just got forked")
 
-
 class ImageInterpreter():
     def __init__(self,config) -> None:
         self.config = common.OpenYaml(config,'image_interpreter')
         self.img_formats = self.config['image_formats']
         self.image_field = self.config['image_field']
-        self.device = common.Device()
         self.use_local_model = self.config['use_local_model']
         self.max_new_tokens = self.config['max_new_tokens']
-        if not self.use_local_model:
+        
+        if self.use_local_model:
+            import torch
+            from transformers import BlipProcessor, BlipForConditionalGeneration
+            self.device = common.Device()
+            self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+            self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", torch_dtype=torch.float16).to(self.device)
+        else:
+            from openai import OpenAI
             self.model = self.config['model']
             self.api_key = os.getenv('OPENAIAPIKEY')
             self.client = OpenAI(api_key=self.api_key)
-
-
 
     def GetInterpretation(self, image_path: str):
         if self.use_local_model:
@@ -35,6 +34,7 @@ class ImageInterpreter():
             return self._openai_interpretation(image_path)
 
     def _local_interpretation(self, image_path: str):
+        import torch
         raw_image = Image.open(requests.get(image_path, stream=True).raw).convert('RGB')
         text = "a photography of"
         inputs = self.processor(raw_image, text, return_tensors="pt").to("mps", torch.float16)
@@ -69,23 +69,9 @@ class ImageInterpreter():
             print(f"Error processing image: {error}")
             raise
 
-            
-
     def Handler(self, posts) -> dict:
-        # initializing the model in the handler so we dont load it everytime we need to caption an image
-        self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-        self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", torch_dtype=torch.float16).to(self.device)
         for post in posts:
             if (self.image_field in post) and (common.EndsWith(post[self.image_field],self.img_formats)):
                 post['image_description'] = f'The posts contains an image that can be described as {self.GetInterpretation(post[self.image_field])}'
         common.DumpToJson('posts.json',data=posts)
         return posts
-
-        
-
-
-
-
-
-
-
